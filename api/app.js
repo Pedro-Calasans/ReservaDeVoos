6e764e3e73ef4c64b778df7a5787d6826155b2a6
Pedro Calasans <pedro.calasans@aluno.ifsp.edu.br>
2022-07-14 00:18:30 -0300

    adicionado Rabbitmq

--- api/app.js
+++ api/app.js
@@ -3,9 +3,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require("cors");
+require("dotenv").config;
 var indexRouter = require('./routes/index');
 var app = express();
 
+//mongo DB
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+var db = mongoose.connection;
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,5 +25,9 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+db.on("error", console.error.bind(console,"connection error:"));
+db.once("open", _ =>{
+    console.log("Conectado");
+})
 
 module.exports = app;
